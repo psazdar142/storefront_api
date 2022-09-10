eb08fdc5476b8d6e71521be3f1c519b519fab58d
Sazdar, Parssa (ps198p) <ps198p@att.com>
2022-09-10 16:39:08 -0500

    Unit testing works on user model

---
+++ src/specs/user_spec.ts
@@ -0,0 +1,28 @@
+// import { UserStore } from "../models/user";
+
+// require { UserStore } from "../models/user";\
+const {UserStore} = require("../models/user") 
+
+const store = new UserStore()
+
+describe("User Model", () => {
+    it('should have an index method', () => {
+      expect(store.index).toBeDefined();
+    });
+  
+    it('should have a show method', () => {
+      expect(store.show).toBeDefined();
+    });
+  
+    it('should have a create method', () => {
+      expect(store.create).toBeDefined();
+    });
+  
+    // it('should have a update method', () => {
+    //   expect(store.index).toBeDefined();
+    // });
+  
+    it('should have a delete method', () => {
+      expect(store.delete).toBeDefined();
+    });
+  });
